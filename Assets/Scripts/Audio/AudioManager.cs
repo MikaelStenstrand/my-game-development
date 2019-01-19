8f7249b12cd6b34a6dd783f2b9c0da70c9421ccb
Mikael Stenstrand <stenstrand.mikael@gmail.com>
2019-01-19 21:01:44 +0200

    added audioMaanger and core sounds

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour	{
+
+    public Sound[] sounds;
+
+    void Awake() {
+        foreach(Sound sound in sounds) {
+            sound.source = gameObject.AddComponent<AudioSource>();
+            sound.source.clip = sound.audioClip;
+            sound.source.volume = sound.volume;
+            sound.source.pitch = sound.pitch;
+            sound.source.loop = sound.loop;
+        }
+    }
+
+    void Start() {
+        foreach(Sound sound in sounds) {
+            if(sound.playOnStart) {
+                Play(sound.name);
+            }
+        }
+    }
+
+    public void Play(string name) {
+        Sound soundToPlay = Array.Find(sounds, (sound) => sound.name == name);
+        if (soundToPlay == null) {
+            Debug.LogWarning("AudioManager:Play: trying to play sound that does not exist: " + name);
+            return;
+        }
+        soundToPlay.source.Play();
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour	{
+    
+    void Start() {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E)) {
+            Interact();
+        }
+    }
+
+    void Interact() {
+        FindObjectOfType<AudioManager>().Play("Violin");
+        Debug.Log("interact");
+    }
+}
