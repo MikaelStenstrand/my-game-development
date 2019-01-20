b7f69e17984f634ec0a89d71009893f9c2a29bcb
Mikael Stenstrand <stenstrand.mikael@gmail.com>
2019-01-20 21:42:21 +0200

    added more cameras to scene

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour	{
+    
+    [Header("Features")]
+    public bool followPlayer;
+    
+    [Header("References")]
+    public Transform playerTransform;
+
+    void Start() {
+        
+    }
+
+    void Update() {
+        if (followPlayer)   {
+            followPlayerActions();
+        }
+    }
+
+    void followPlayerActions()   {
+        if (playerTransform != null)
+            transform.LookAt(playerTransform);
+    }
+
+
+
+}
