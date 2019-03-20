ef85f0abac16282cb543a320b5c8ff06f962beab
Mikael Stenstrand <stenstrand.mikael@gmail.com>
2019-03-20 18:18:27 +0200

    add flashlight sound

--- Assets/Scripts/Actions/FlashLightAction.cs
+++ Assets/Scripts/Actions/FlashLightAction.cs
@@ -2,18 +2,29 @@
 
 public class FlashLightAction : MonoBehaviour {
 
-    [SerializeField] private GameObject flashLight;
+    [SerializeField] private GameObject flashlight;
+    [SerializeField] private Sound soundEffect;
+    [SerializeField] private float soundVolume = 0.2f;
+
     private EquipmentManager equipmentManager;
+    private AudioSource audioSource;
+
 
     private void Start() {
         equipmentManager = EquipmentManager.instance;
+        audioSource = gameObject.AddComponent<AudioSource>();
     }
 
 
     public void SwitchLightState() {
         if (equipmentManager.isEquiped(gameObject)) {
-            bool state = flashLight.activeSelf;
-            flashLight.SetActive(!state);
+            bool state = flashlight.activeSelf;
+            flashlight.SetActive(!state);
+            this.PlaySwitchLightSound();
         }
+    }
+
+    private void PlaySwitchLightSound() {
+        audioSource.PlayOneShot(soundEffect.audioClip, soundVolume);
     }
 }
