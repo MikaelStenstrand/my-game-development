6676dd5a7c722dc2b5e6e354ac1a40c5bc9cc4f5
Mikael Stenstrand <stenstrand.mikael@gmail.com>
2019-03-20 21:34:58 +0200

    add interaction for opening the room01 door

--- Assets/Scripts/Actions/OpenDoorAction.cs
+++ Assets/Scripts/Actions/OpenDoorAction.cs
@@ -2,38 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TransformAction : ActionLogic	{
+public class OpenDoorAction : MonoBehaviour	{
+
+    [SerializeField] Transform targetTransform;
 
     [SerializeField] 
-    Transform targetTransform;
-
-    [SerializeField]
     [Tooltip("Make changes in relation to the target transform.")]
     bool relatedChanges;
 
-    [SerializeField]
-    float transitionSpeed = 1.0f;
+    [SerializeField] float transitionSpeed = 1.0f;
 
-    [SerializeField]
-    Vector3 newPosition;
-    [SerializeField]
-    Vector3 newRotation;
-    [SerializeField]
-    Vector3 newScale;
+    [SerializeField] Vector3 newPosition;
+    [SerializeField] Vector3 newRotation;
+    [SerializeField] Vector3 newScale;
 
-    public override void DoAction() {
-        Debug.Log("Do Transform action");
+    public void OpenDoor() {
+        Debug.Log("Open door");
 
         Vector3 positionChange = (relatedChanges) ? targetTransform.localPosition + newPosition : newPosition;
         Vector3 rotationChange = (relatedChanges) ? targetTransform.localEulerAngles + newRotation : newRotation;
         Vector3 scaleChange = (relatedChanges) ? targetTransform.localScale + newScale : newScale;
         StartCoroutine(SmoothTransform(targetTransform, positionChange, rotationChange, scaleChange, 1 / transitionSpeed));
     }
-
-    public override void FailedToDoAction() {
-        Debug.Log("failed to do transform action");
-    }
-
 
     IEnumerator SmoothTransform(Transform target, Vector3 position, Vector3 rotation, Vector3 scale, float time) {
         float elapsedTime = 0f;
@@ -46,9 +36,11 @@ public class TransformAction : ActionLogic	{
             target.localEulerAngles = Vector3.Lerp(startingRotation, rotation, (elapsedTime / time));
             target.localScale = Vector3.Lerp(startingScale, scale, (elapsedTime / time));
             elapsedTime += Time.deltaTime;
-           yield return new WaitForSeconds(.05f);
+            yield return new WaitForSeconds(.05f);
         }
     }
+
+
 
 
 }
--- Assets/Scripts/ToolInteraction.cs
+++ Assets/Scripts/ToolInteraction.cs
@@ -2,28 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(ActionLogic))]
 public class ToolInteraction : Interactable	{
 
     // equipment required to interact with this object
     public Equipment requiredEquipment;
-
-
     EquipmentManager equipmentManager;
-    ActionLogic action;
 
     private void Start() {
         equipmentManager = EquipmentManager.instance;
-        action = GetComponent<ActionLogic>();
     }
 
     public override bool Interact() {
         if (CheckEquipmentOnPlayer()) {
-            action.DoAction();
             return true;
         }
         Debug.Log("Player has not equiped the required tool");
-        action.FailedToDoAction();
         return false;
     }
 
