f427635c429a3e48dac5879a4b063297dfef2b4f
Mikael Stenstrand <stenstrand.mikael@gmail.com>
2019-03-24 16:02:14 +0200

    add simple sound player component

---
+++ Assets/Scripts/Audio/SimpleSoundPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleSoundPlayer : MonoBehaviour	{
+
+
+    [SerializeField] Sound sound;
+
+    [Range(0.0f, 1.0f)]
+    [SerializeField] float soundVolume = 0.2f;
+
+    private AudioSource audioSource;
+
+    private void Start() {
+        audioSource = gameObject.AddComponent<AudioSource>();
+    }
+
+    public void PlaySound() {
+        audioSource.PlayOneShot(sound.audioClip, soundVolume);
+    }
+
+
+}
