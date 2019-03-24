578c4d08d0c7e0ec5154b47b88ac5c36f28b8832
Mikael Stenstrand <stenstrand.mikael@gmail.com>
2019-03-24 16:03:01 +0200

    added ForceArea component with fan state

---
+++ Assets/Scripts/ForceArea.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceArea : MonoBehaviour	{
+
+    [SerializeField] BoolReference fanState;
+    [SerializeField] float forceEffect = 1;
+    Rigidbody targetedRigidbody = null;
+
+    private void FixedUpdate() {
+        if (fanState.Value && targetedRigidbody != null) {
+            AddForce(targetedRigidbody);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        Debug.Log("ENTER");
+        //if (other.tag == "Player") {
+            if (targetedRigidbody == null)
+                targetedRigidbody = other.attachedRigidbody;
+        //}
+    }
+
+    private void OnTriggerExit(Collider other) {
+        Debug.Log("EXIT");
+        //if (other.tag == "Player") {
+            targetedRigidbody = null;
+        //}    
+    }
+
+
+    private void AddForce(Rigidbody rigidbody) {
+        Debug.Log("FORCE");
+        rigidbody.AddForce(- Vector3.forward * Time.deltaTime * forceEffect);
+    }
+}
