77594ceb10decd320500baa26d1d7b8f743b7b99
Mikael Stenstrand <stenstrand.mikael@gmail.com>
2019-03-28 22:21:22 +0200

    add Fan Force to affect character

--- Assets/Scripts/ForceArea.cs
+++ Assets/Scripts/ForceArea.cs
@@ -7,31 +7,47 @@ public class ForceArea : MonoBehaviour	{
     [SerializeField] BoolReference fanState;
     [SerializeField] float forceEffect = 1;
     Rigidbody targetedRigidbody = null;
+    CharacterController targetedCharacterController = null;
+    Vector3 impact = Vector3.zero;
 
     private void FixedUpdate() {
-        if (fanState.Value && targetedRigidbody != null) {
-            AddForce(targetedRigidbody);
+        if (fanState.Value) {
+            if (targetedRigidbody != null) {
+                AddForce(targetedRigidbody);
+            }
+            if (impact.magnitude > 0.2F && targetedCharacterController != null)
+                targetedCharacterController.Move(impact * Time.deltaTime);
+
+            impact = Vector3.Lerp(impact, Vector3.zero, 5 * Time.deltaTime);
         }
     }
 
     private void OnTriggerEnter(Collider other) {
-        Debug.Log("ENTER");
-        //if (other.tag == "Player") {
-            if (targetedRigidbody == null)
-                targetedRigidbody = other.attachedRigidbody;
-        //}
+        if (targetedRigidbody == null) {
+            targetedRigidbody = other.attachedRigidbody;
+            targetedCharacterController = other.GetComponent<CharacterController>();
+        }
     }
 
     private void OnTriggerExit(Collider other) {
-        Debug.Log("EXIT");
-        //if (other.tag == "Player") {
-            targetedRigidbody = null;
-        //}    
+        targetedRigidbody = null;
+        targetedCharacterController = null;
     }
 
 
     private void AddForce(Rigidbody rigidbody) {
-        Debug.Log("FORCE");
-        rigidbody.AddForce(- Vector3.forward * Time.deltaTime * forceEffect);
+        rigidbody.AddForce(Vector3.forward * Time.deltaTime * forceEffect);
+        if (targetedCharacterController != null)
+            AddForce(targetedCharacterController);
     }
+
+    private void AddForce(CharacterController characterController) {
+        if (characterController != null) {
+            Vector3 dir = Vector3.forward;
+            dir.Normalize();
+            if (dir.y < 0) dir.y = -dir.y;
+            impact += dir.normalized * forceEffect;
+        }
+    }
+
 }
