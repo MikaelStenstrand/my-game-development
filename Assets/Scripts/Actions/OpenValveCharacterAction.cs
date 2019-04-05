5f20ea8f9cab3d5a92e2415648581bab050e19b9
Mikael Stenstrand <stenstrand.mikael@gmail.com>
2019-04-06 01:15:18 +0300

    add functionality to open valve

---
+++ Assets/Scripts/Actions/OpenValveCharacterAction.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenValveCharacterAction : MonoBehaviour	{
+
+    [SerializeField] Sound hitSound;
+
+    [Range(0.0f, 1.0f)]
+    [SerializeField] float soundVolume = 0.2f;
+
+    [SerializeField] BoolReference steamState;
+
+    private Animator animator;
+    private AudioSource audioSource;
+
+    void Start() {
+        animator = gameObject.GetComponent<Animator>();
+        audioSource = gameObject.AddComponent<AudioSource>();
+    }
+
+    public void TriggerOpenValveAnimation() {
+        animator.SetTrigger("OpenValve");
+    }
+    public void PlayOpenValveSound() {
+        audioSource.PlayOneShot(hitSound.audioClip, soundVolume);
+        steamState.Value = !steamState.Value;
+    }
+
+}
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -129,5 +129,4 @@ public class PlayerController : MonoBehaviour	{
             useEquippedToolEvent.Raise();
         }
     }
-
 }
