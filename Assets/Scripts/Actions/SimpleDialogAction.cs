1c6890704b5dca1b1abd0df23165416235ef81ba
Mikael Stenstrand <stenstrand.mikael@gmail.com>
2019-04-05 22:26:54 +0300

    add SimpleDialogAction with single or double VIDE dialogs

---
+++ Assets/Scripts/Actions/SimpleDialogAction.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class SimpleDialogAction : MonoBehaviour	{
+
+    [Header("Single Dialog")]
+    [SerializeField] VIDE_Assign singleDialog = null;
+
+    [Header("Success & Fail Dialogs")]
+    [SerializeField] VIDE_Assign successDialog = null;
+    [SerializeField] VIDE_Assign failDialog = null;
+
+    private AudioSource audioSource;
+
+    private void Awake() {
+        audioSource = gameObject.AddComponent<AudioSource>();
+    }
+
+    public void StartSingleDialog() {
+        if (singleDialog != null)
+            DialogManager.instance.StartDialog(singleDialog, audioSource);
+    }
+
+    public void StartSuccessDialog() {
+        if (successDialog != null)
+            DialogManager.instance.StartDialog(successDialog, audioSource);
+    }
+
+    public void StartFailDialog() {
+        if (failDialog != null)
+            DialogManager.instance.StartDialog(failDialog, audioSource);
+    }
+
+
+
+
+
+
+}
