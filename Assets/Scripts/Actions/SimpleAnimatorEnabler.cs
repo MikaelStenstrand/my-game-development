87c562cd9789fdca43fcf0b47083b80c987e196b
Mikael Stenstrand <mikael.k.stenstrand@gmail.com>
2019-04-22 18:12:48 +0300

    add SimpleAnimatorEnabler and SwitchInteractable

---
+++ Assets/Scripts/Actions/SimpleAnimatorEnabler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleAnimatorEnabler : MonoBehaviour {
+
+    [SerializeField] BoolReference isEnabled;
+    [SerializeField] Animator animator = null;
+
+    private bool currentState;
+
+    void Start() {
+        if (animator == null)
+            animator = gameObject.GetComponent<Animator>();
+        currentState = animator.enabled;
+    }
+
+    void Update() {
+        if (currentState != isEnabled.Value && animator != null) {
+            animator.enabled = isEnabled.Value;
+            currentState = isEnabled.Value;
+        }
+    }
+}
---
+++ Assets/Scripts/Triggers/SwitchInteractable.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchInteractable : Interactable {
+
+    [SerializeField] GameEvent gameEventOn;
+    [SerializeField] GameEvent gameEventOff;
+
+    [SerializeField] BoolReference switchState;
+
+    public override bool Interact() {
+        if (switchState.Value) {
+            if (gameEventOff != null) {
+                Debug.Log("Switching off");
+                switchState.Value = false;
+                gameEventOff.Raise();
+                return true;
+            }
+        } else {
+            if (gameEventOn != null) {
+                Debug.Log("Switching on");
+                switchState.Value = true;
+                gameEventOn.Raise();
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+
+}
